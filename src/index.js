0bf36dd153747649d10db87ef7a654a4e1e220c3
sophiepavlova <olni.pavlova@gmail.com>
2025-06-02 11:23:41 -0700

    Update code for dynamic routes

--- src/index.js
+++ src/index.js
@@ -24,6 +24,57 @@ app.get('/hello', (request, reply) => {
   } else reply.send(`Hello, ${name}!`);
 });
 
+// app.get('/courses/:id', (req, res) => {
+//   res.send(`Course ID: ${req.params.id}`)
+// })
+
+// app.get('/users/:id', (req, res) => {
+//   res.send(`User ID: ${req.params.id}`)
+// })
+
+const state = {
+  users: [
+    {
+      id: 1,
+      name: 'user',
+    },
+  ],
+}
+
+// app.get('/users/:id', (req, res) => {
+//   const { id } = req.params
+//   const user = state.users.find(user => user.id === parseInt(id))
+//   if (!user) {
+//     res.code(404).send({ message: 'User not found' })
+//   }
+//   else {
+//     res.send(user)
+//   }
+// })
+
+app.get('/courses/:courseId/lessons/:id', (req, res) => {
+  res.send(`Course ID: ${req.params.courseId}; Lesson ID: ${req.params.id}`)
+})
+
+// app.get('/users/:id/post/:postId', (req, res) => {
+//   res.send(`User ID: ${req.params.id}; Post ID: ${req.params.postId}`)
+// })
+
+app.get('/users/:id/post/:postId', (req, res) => {
+  const { id, postId } = req.params;
+  res.send(`User ID: ${id}; Post ID: ${postId}`);
+});
+
+
+// users/{id}/post/{postId}
+app.get('/courses/new', (req, res) => {
+  res.send('Course build')
+})
+
+app.get('/courses/:id', (req, res) => {
+  res.send(`Course ID: ${req.params.id}`)
+})
+
 
 app.listen({ port }, (err) => {
   if (err) {
